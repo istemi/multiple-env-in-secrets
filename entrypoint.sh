74320f261ced0d4b4baef011b7375559274f98c7
Erol Kavas <erol@erol.ca>
2021-03-11 06:53:35 -0500

    update multiline string

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -l
 export_env_var=$(echo "$1" | awk '{print "export " $0}')
+
+export_env_var="${export_env_var//'%'/'%25'}"
+export_env_var="${export_env_var//$'\n'/'%0A'}"
+export_env_var="${export_env_var//$'\r'/'%0D'}"
+
 echo "::set-output name=time::$export_env_var"
